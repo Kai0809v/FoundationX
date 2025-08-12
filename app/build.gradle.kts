35050ced2226e793edba622be57738f5f5e95ea7
Kai <kai0809v@gmail.com>
2025-08-12 17:25:36 +0800

    遇到了前所未有的bug，导致打开as时项目一直不能导入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,17 @@ plugins {
 
 android {
     namespace = "com.xcu.kai"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.xcu.kai"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        signingConfig = signingConfigs.getByName("debug")
     }
 
     buildTypes {
@@ -32,6 +33,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    buildToolsVersion = "36.0.0"
 }
 
 dependencies {
@@ -45,6 +47,7 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.annotation)
     implementation(libs.activity)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
